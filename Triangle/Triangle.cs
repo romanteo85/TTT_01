80d570640659642c6ebd4e2fa3dac1d8923cbe43
romanteo85 <romanteo85@gmail.com>
2016-04-12 13:35:04 +0300

    Added files via upload

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Triangle
+    {
+        public double LengthA { get; }
+        public double LengthB { get; }
+        public double LengthC { get; }
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0) throw new ArgumentOutOfRangeException(nameof(a), "Значение длины должно быть положительным числом");
+            if (b <= 0) throw new ArgumentOutOfRangeException(nameof(b), "Значение длины должно быть положительным числом");
+            if (c <= 0) throw new ArgumentOutOfRangeException(nameof(c), "Значение длины должно быть положительным числом");
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+                throw new ArithmeticException("Треугольник с указанными размерами не возможен");
+
+            LengthA = a;
+            LengthB = b;
+            LengthC = c;
+        }
+
+        public double CalcSquare()
+        {
+            double p = (LengthA + LengthB + LengthC) / 2;
+            return Math.Sqrt(p * (p - LengthA) * (p - LengthB) * (p - LengthC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double maxSideLength = Math.Max(Math.Max(LengthA, LengthB), LengthC);
+            return Math.Abs(LengthA * LengthA + LengthB * LengthB + LengthC * LengthC - 2 * maxSideLength * maxSideLength) < double.Epsilon;
+        }
+    }
+}
---
+++ TriangleUnitTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+
+namespace TriangleUnitTests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ZeroLengthTest()
+        {
+            Triangle triangle = new Triangle(0, 4, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeLengthTest()
+        {
+            Triangle triangle = new Triangle(-3, 4, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void InvalidLengthsTest()
+        {
+            Triangle triangle = new Triangle(3, 4, 8);
+        }
+
+        [TestMethod]
+        public void RightTriangleTest()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            Assert.IsTrue(triangle.IsRightTriangle());
+        }
+
+        [TestMethod]
+        public void CorrertSquareTest()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            double square = triangle.CalcSquare();
+            Assert.AreEqual(square, 6, double.Epsilon, "Получившееся значение площади не совпадает с ожидаемым");
+        }
+    }
+}
